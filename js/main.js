62636bc5b46fc52560c7a045210ed7e109c497bd
Jaden Bertino <jaden@bertinofamily.com>
2023-02-06 23:05:57 -0800

    organize renderEntry function

--- js/main.js
+++ js/main.js
@@ -4,47 +4,47 @@
 
 */
 
-const $newEntryForm = document.querySelector('#new-entry-form')
-const $newEntryImg = document.querySelector('.new-entry img')
-const $newEntryPhotoURL = document.querySelector('#new-entry-photoURL')
+const $newEntryForm = document.querySelector('#new-entry-form');
+const $newEntryImg = document.querySelector('.new-entry img');
+const $newEntryPhotoURL = document.querySelector('#new-entry-photoURL');
 
 function loadImg(src) {
   return new Promise((resolve, reject) => {
-    const img = new Image()
-    img.onload = () => resolve(img)
-    img.onerror = reject
-    img.src = src
-  })
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.onerror = reject;
+    img.src = src;
+  });
 }
 
 // Photo URL input updates new entry preview img
-$newEntryPhotoURL.addEventListener('input', async (e) => {
+$newEntryPhotoURL.addEventListener('input', async e => {
   try {
-    const img = await loadImg($newEntryPhotoURL.value)
-    $newEntryImg.setAttribute('src', img.src)
+    const img = await loadImg($newEntryPhotoURL.value);
+    $newEntryImg.setAttribute('src', img.src);
   } catch {
     // invalid img url
-    $newEntryImg.setAttribute('src', "./images/placeholder-image-square.jpg")
+    $newEntryImg.setAttribute('src', './images/placeholder-image-square.jpg');
   }
-})
+});
 
 // Form Validation Helpers
 function checkIfAlphanumeric(char) {
-  if (typeof char !== 'string') return false
-  if (char >= 0 && char <= 9) return true // number
-  if (char.toLowerCase() !== char.toUpperCase()) return true // alphabetical
-  return false // not alphanumeric
+  if (typeof char !== 'string') return false;
+  if (char >= 0 && char <= 9) return true; // number
+  if (char.toLowerCase() !== char.toUpperCase()) return true; // alphabetical
+  return false; // not alphanumeric
 }
 
 function checkIfValidEntryInput(str) {
-  if (typeof str !== 'string') return false 
+  if (typeof str !== 'string') return false;
 
   // has at least 1 alphanumeric char
-  return !!str.split('').filter(char => checkIfAlphanumeric(char)).length
+  return !!str.split('').filter(char => checkIfAlphanumeric(char)).length;
 }
 
 // Form Submit & Validation
-$newEntryForm.addEventListener('submit', async (e) => {
+$newEntryForm.addEventListener('submit', async e => {
   /*
   create newEntry object to store valid formControl name + values
   iterate through formControls
@@ -54,47 +54,50 @@ $newEntryForm.addEventListener('submit', async (e) => {
   check that newEntry Object is same length as # of form controls. means that all form controls were valid
     if true then push newEntry to data
   */
-  e.preventDefault()
-  const newEntry = { entryId: data.nextEntryId }
-  
+  e.preventDefault();
+  const newEntry = { entryId: data.nextEntryId };
+
   for (let formControl of $newEntryForm) {
-    if (!formControl.name) continue // skip button
-    
+    if (!formControl.name) continue; // skip button
+
     // img url
-    if (formControl.name === 'photoURL') { 
+    if (formControl.name === 'photoURL') {
       try {
-        const {src} = await loadImg(formControl.value)
-        newEntry.photoURL = src
-        $newEntryForm.elements["new-entry-photoURL"].style.outline = "inherit"
+        const { src } = await loadImg(formControl.value);
+        newEntry.photoURL = src;
+        $newEntryForm.elements['new-entry-photoURL'].style.outline = 'inherit';
       } catch {
-        $newEntryForm.elements["new-entry-photoURL"].style.outline = "medium solid red"
+        $newEntryForm.elements['new-entry-photoURL'].style.outline =
+          'medium solid red';
       }
-    } 
-    
+    }
+
     // title + notes
-    else { 
-      const isValidEntry = checkIfValidEntryInput(formControl.value)
+    else {
+      const isValidEntry = checkIfValidEntryInput(formControl.value);
       if (isValidEntry) {
-        newEntry[formControl.name] = formControl.value
-        $newEntryForm.elements[`new-entry-${formControl.name}`].style.outline = "inherit"
+        newEntry[formControl.name] = formControl.value;
+        $newEntryForm.elements[`new-entry-${formControl.name}`].style.outline =
+          'inherit';
       } else {
-        $newEntryForm.elements[`new-entry-${formControl.name}`].style.outline = "medium solid red"
+        $newEntryForm.elements[`new-entry-${formControl.name}`].style.outline =
+          'medium solid red';
       }
     }
   }
 
   // if newEntry is length 4 then all form controls were pushed aka all form controls were all valid
   if (Object.keys(newEntry).length === $newEntryForm.length) {
-    data.entries.unshift(newEntry)
-    data.nextEntryId++
-    $newEntryImg.setAttribute('src', "./images/placeholder-image-square.jpg")
-    e.target.reset()
-    const $newEntry = renderEntry(newEntry)
-    $viewEntriesList.insertBefore($newEntry, $viewEntriesList.firstChild)
-    setEntryVisibility()
-    viewSwap('entries')
+    data.entries.unshift(newEntry);
+    data.nextEntryId++;
+    $newEntryImg.setAttribute('src', './images/placeholder-image-square.jpg');
+    e.target.reset();
+    const $newEntry = renderEntry(newEntry);
+    $viewEntriesList.insertBefore($newEntry, $viewEntriesList.firstChild);
+    setEntryVisibility();
+    viewSwap('entries');
   }
-})
+});
 
 /*
 
@@ -117,77 +120,88 @@ function renderEntry(entry) {
 
   */
 
-  const li = document.createElement('li')
-  const imgWrapper = document.createElement('div')
-  const entryImg = document.createElement('img')
-  const textWrapper = document.createElement('div')
-  const entryTitle = document.createElement('h3')
-  const entryTitleText = document.createTextNode(entry.title)
-  const entryDescription = document.createElement('p')
-  const entryDescriptionText = document.createTextNode(entry.notes)
+  const li = document.createElement('li');
+  const imgWrapper = document.createElement('div');
+  const entryImg = document.createElement('img');
+  const textWrapper = document.createElement('div');
+  const headerWrapper = document.createElement('div');
+  const entryTitle = document.createElement('h3');
+  const entryTitleText = document.createTextNode(entry.title);
+  const pencilIcon = document.createElement('i');
+  const entryDescription = document.createElement('p');
+  const entryDescriptionText = document.createTextNode(entry.notes);
 
-  imgWrapper.setAttribute('class', 'img-wrapper')
-  entryImg.setAttribute('src', entry.photoURL)
-  textWrapper.setAttribute('class', 'text-wrapper')
-  entryTitle.appendChild(entryTitleText)
-  entryDescription.appendChild(entryDescriptionText)
-  li.appendChild(imgWrapper)
-  imgWrapper.appendChild(entryImg)
-  li.appendChild(textWrapper)
-  textWrapper.appendChild(entryTitle)
-  textWrapper.appendChild(entryDescription)
+  // img
+  imgWrapper.setAttribute('class', 'img-wrapper');
+  entryImg.setAttribute('src', entry.photoURL);
+  imgWrapper.appendChild(entryImg);
+  
+  // header wrapper
+  textWrapper.appendChild(headerWrapper);  
+  headerWrapper.appendChild(entryTitle)
+  headerWrapper.appendChild(pencilIcon)
+  entryTitle.appendChild(entryTitleText);
 
-  return li
+  // text wrapper
+  textWrapper.setAttribute('class', 'text-wrapper');
+  textWrapper.appendChild(entryDescription);
+  entryDescription.appendChild(entryDescriptionText);
+
+  // li
+  li.appendChild(textWrapper);
+  li.appendChild(imgWrapper);
+
+  return li;
 }
 
-const $viewEntries = document.querySelector('.view-entries')
-const $viewEntriesList = document.querySelector('.view-entries ul')
-const $noEntries = document.querySelector('#no-entries')
+const $viewEntries = document.querySelector('.view-entries');
+const $viewEntriesList = document.querySelector('.view-entries ul');
+const $noEntries = document.querySelector('#no-entries');
 
 function setEntryVisibility() {
   // Display entries if there are any
   if (data.entries.length) {
-    $noEntries.classList.add('hidden')
-    $viewEntriesList.classList.remove('hidden')
+    $noEntries.classList.add('hidden');
+    $viewEntriesList.classList.remove('hidden');
   }
-  
+
   // Display "no entries" text if none available
   else {
-    $noEntries.classList.remove('hidden')
-    $viewEntriesList.classList.add('hidden')
+    $noEntries.classList.remove('hidden');
+    $viewEntriesList.classList.add('hidden');
   }
 }
-setEntryVisibility()
+setEntryVisibility();
 
 // Render all current entries upon page load
 data.entries.forEach(entry => {
-  const newEntry = renderEntry(entry)
-  $viewEntriesList.appendChild(newEntry)
-})
+  const newEntry = renderEntry(entry);
+  $viewEntriesList.appendChild(newEntry);
+});
 
 // View Swapping
 const $views = [
   document.querySelector('div[data-view="entry-form"'),
   document.querySelector('div[data-view="entries"')
-]
+];
 
 function viewSwap(viewName) {
   $views.forEach(view => {
     if (view.getAttribute('data-view') === viewName) {
-      view.classList.remove('hidden')
+      view.classList.remove('hidden');
     } else {
-      view.classList.add('hidden')
+      view.classList.add('hidden');
     }
-  })
-  data.view = viewName
+  });
+  data.view = viewName;
 }
-const prevSessionView = data.view
-viewSwap(prevSessionView)
+const prevSessionView = data.view;
+viewSwap(prevSessionView);
 
-const $newEntryBtn = document.querySelector('#new-entry-btn') 
-const $viewEntriesBtn = document.querySelector('#view-entries-btn')
-const $navViewEntriesBtn = document.querySelector('header button')
+const $newEntryBtn = document.querySelector('#new-entry-btn');
+const $viewEntriesBtn = document.querySelector('#view-entries-btn');
+const $navViewEntriesBtn = document.querySelector('header button');
 
-$newEntryBtn.addEventListener('click', () => viewSwap('entry-form'))
-$viewEntriesBtn.addEventListener('click', () => viewSwap('entries'))
-$navViewEntriesBtn.addEventListener('click', () => viewSwap('entries'))
+$newEntryBtn.addEventListener('click', () => viewSwap('entry-form'));
+$viewEntriesBtn.addEventListener('click', () => viewSwap('entries'));
+$navViewEntriesBtn.addEventListener('click', () => viewSwap('entries'));
